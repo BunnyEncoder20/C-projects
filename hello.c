142c9f30400c6465595def518163e34c0e33ecbf
Varun Verma <varun.verma2024@gmail.com>
2025-12-12 14:03:19 +0530

    ritual completed

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void nums() {
+  int num = 6;
+  printf("Number: %d", num);
+  float deciNum = 10.5;
+  printf("\nDecimal Number: %.2f", deciNum);
+}
+
+int main() {
+  printf("Hello World\n");
+  nums();
+}
